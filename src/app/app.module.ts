507c1972785fcfdf107418ddafdff8a7bd613928
hansakab <hmtbv8@gmail.com>
2021-05-27 09:58:54 +0530

    project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{NgxAgoraModule} from 'ngx-agora';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxAgoraModule.forRoot({AppID: 'insert your agora project id here'})
   ],
   providers: [],
   bootstrap: [AppComponent]
